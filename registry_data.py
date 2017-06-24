6adf4f3f3646eadd3c145d9d425dfdeb6521adde
Ben Cipollini <bcipollini@classy.org>
2017-06-24 07:32:33 -0700

    RF: create function for streaming data.

--- registry_data.py
+++ registry_data.py
@@ -1,35 +1,41 @@
 import pandas as pd
-from eventregistry import *
-
-API_KEY = "21c52a6d-4ce5-48b7-98df-7c00c27f866a"
-er = EventRegistry(apiKey = API_KEY)
-q = QueryEventsIter(lang='eng',
-                    minArticlesInEvent=500)
-
-# collects event ids
-event_data = []
-for event in q.execQuery(er, #sortBy = "date",
-                        maxItems=1001):
-    event_data.append(event)
-df_event = pd.DataFrame(event_data)
-event_uris = df_event.uri.tolist()
+import eventregistry as er
 
 
-event_uris = [ev for ev in event_uris if ev[:3] == 'eng']
+def fetch_events(api_key, min_articles=500, csv_file=None):
+    event_registry = er.EventRegistry(apiKey=api_key)
+    qei = er.QueryEventsIter(
+        lang='eng', minArticlesInEvent=min_articles)
 
-all_articles = []
-for uri in event_uris:
-    print "current uri: ", uri
-    current_event_data = []
-    
-    iter = QueryEventArticlesIter(uri)
-    for article in iter.execQuery(er, lang = "eng"):
-        current_event_data.append(article)
-        
-    tmp_df = pd.DataFrame(current_event_data)
-    print "shape of df: {}".format(tmp_df.shape)
-    print "unique url: {}".format(len(set(tmp_df['url'])))
-    all_articles.append(tmp_df)
-    
+    # collects event ids
+    event_data = []
+    for event in qei.execQuery(event_registry, maxItems=1001):
+        event_data.append(event)
+    df_event = pd.DataFrame(event_data)
+    event_uris = df_event.uri.tolist()
 
-final_df = pd.concat(all_articles)
+    event_uris = [ev for ev in event_uris if ev[:3] == 'eng']
+
+    all_articles = []
+    for uri in event_uris:
+        print "current uri: ", uri
+        current_event_data = []
+
+        query_iter = er.QueryEventArticlesIter(uri)
+        for article in query_iter.execQuery(er, lang="eng"):
+            current_event_data.append(article)
+
+        tmp_df = pd.DataFrame(current_event_data)
+        print "shape of df: {}".format(tmp_df.shape)
+        print "unique url: {}".format(len(set(tmp_df['url'])))
+        all_articles.append(tmp_df)
+
+    final_df = pd.concat(all_articles)
+
+    if csv_file:
+        final_df.to_csv(csv_file, encoding='utf-8')
+    return final_df
+
+
+if __name__ == '__main__':
+    fetch_events(csv_file='raw_dataframe.csv', api_key="21c52a6d-4ce5-48b7-98df-7c00c27f866a")
