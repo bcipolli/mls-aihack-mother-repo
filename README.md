b5299eb1caf349841dee2865d711e222d24ae27b
Ben Cipollini <bcipollini@classy.org>
2017-06-23 23:28:25 -0700

    Add nlp_demo.py, requirements.txt, and docs.

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
-Goal:
+** Goal:
+
 * Match news articles
 * Find common text in the articles, and remove
 * Cluster "residual words" via topic modeling
 * Apply "topics" to all residuals from a news source, to get their "bias"
 
+
+** Setup & Installation:
+
+```
+pip install -r requirements.txt
+python -c "import nltk; nltk.download()"
+```
+
+Then choose to install the "popular" collection.
+
+
+** NLP Demo
+
+This demo shows stemming, lemmatizing, and word counting (including tf-idf)
+
+```
+python nlp_demo.py
+```
---
+++ nlp_demo.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+from nltk.stem import PorterStemmer, WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+
+
+def lemmatize(docs):
+    # Stem words (normalize words within the same part of speech)
+    # lemmatize words (normalize words across parts of speech)
+    stemmer = PorterStemmer()
+    lemmatizer = WordNetLemmatizer()
+
+    stemmed_docs = []
+    for doc in docs:
+        words = [w.strip() for w in doc.split(' ') if w.strip()]
+        stem_words = [stemmer.stem(w) for w in words]
+        new_words = [lemmatizer.lemmatize(w) for w in stem_words]
+        stemmed_docs.append(' '.join(new_words))
+    return stemmed_docs
+
+
+def vectorize(docs, type="count", min_word_length=3, min_df=1):
+    if type == "count":
+        # Count words
+        cls = CountVectorizer
+    elif type == "tfidf":
+        # Count words, normalize by frequency
+        cls = TfidfVectorizer
+    else:
+        raise NotImplementedError()
+
+    vectorizer = cls(
+        min_df=min_df, token_pattern=u'(?u)\\b[a-zA-Z]{%d,}\\b' % min_word_length,
+        max_df=np.inf, analyzer='word', stop_words="english",
+        encoding='utf-8', decode_error='ignore')
+
+    vectorizer.fit(docs)
+    X = vectorizer.transform(docs)
+    vocab = np.asarray(vectorizer.vocabulary_.keys())
+    vocab = vocab[np.argsort(vectorizer.vocabulary_.values())]  # ordered by counts (
+
+    return X, vocab
+
+article_text = """
+Police have said they are considering manslaughter charges in relation to the deadly Grenfell Tower blaze as they revealed that both the insulation and tiles at the building failed safety tests.
+Det Supt Fiona McCormack, who is overseeing the investigation, said on Friday that officers had established that the initial cause of the fire was a fridge-freezer and that it was not started deliberately.
+She said they were trying to get to the bottom of why the fire started so quickly. Insulation
+"""
+docs = [article_text]
+l_docs = lemmatize(docs)
+vc_docs = vectorize(l_docs, type="count")
+vt_docs = vectorize(l_docs, type="tfidf")
+
+print "Original text: ", article_text
+print "Stemmed & counted text: ", vc_docs[0].todense(), vc_docs[1]
+print "Stemmed & counted (tfidf) text: ", vt_docs[0].todense(), vt_docs[1]
