ec88ffea7687b6e1587d145542d4f3c70a8909b0
frogman141 <bakeralex664@gmail.com>
2017-06-25 13:38:06 -0700

    finished plotting

--- plotting.py
+++ plotting.py
@@ -1,29 +1,61 @@
 import numpy as np
+import plotly
 import plotly.plotly as py
 import plotly.graph_objs as go
 from plotly.tools import set_credentials_file
 from sklearn.manifold import TSNE
 
 
-def tsne_plotly(data, cat, labels, source, username, api_key, seed=0):
-    print("Plotting data...")
-    set_credentials_file(username=username, api_key=api_key)
-
-    # creating the model --- I am not sure if you can pickle TSNE need to run experiments. Had problems when i pickeled it
-    model = TSNE(n_components=3, random_state=seed, verbose=1)
-    reduced = model.fit_transform(data)
-
-    # scratter plot info for topics
+def gen_plotly_specs(datas, names, cat):
     data = []
 
-    for n in np.unique(cat):
-        i = np.where(cat == n)
-        index = reduced[i]
+    for name in np.unique(cat):
+
+        i = np.where(cat == name)
+        index = datas[i]
 
         # creating scatter plot for a topic
+        trace = go.Scatter3d(
+            x=index[:, 0],
+            y=index[:, 1],
+            z=index[:, 2],
+            mode='markers',
+            marker=dict(
+                size=12,
+                line=dict(
+                    width=0.0
+                ),
+                opacity=0.8
+            ),
+            name=name
+        )
 
         data.append(trace)
 
+    return data
+
+def tsne_plotly(data, cat, labels, source, username, api_key, seed=0):
+    print("Plotting data...")
+    set_credentials_file(username=username, api_key=api_key)
+    model = TSNE(n_components=3, random_state=seed, verbose=1)
+    # creating the model --- I am not sure if you can pickle TSNE need to run experiments. Had problems when i pickeled it
+
+    model = TSNE(n_components=3, random_state=seed, verbose=1)
+    reduced = model.fit_transform(data)
+    plot_params = [[reduced, labels, cat, True], [reduced, labels, source, False]]
+
+    # generating figures
+    figures = []
+    for i in plot_params:
+        if i[3]:
+            fname = 'topics-scatter.html'
+        else:
+            fname = 'source-scatter.html'
+
+        fig = gen_plotly_specs(i[0], i[1], i[2])
+        figures.append([fig, fname])
+
+    # general figure layouts these are default values
     layout = go.Layout(
         margin=dict(
             l=0,
@@ -33,45 +65,9 @@ def tsne_plotly(data, cat, labels, source, username, api_key, seed=0):
         )
     )
 
-    fig = go.Figure(data=data, layout=layout)
-    py.iplot(fig, filename='simple-3d-scatter')
+    for n in figures:
+        plotly.offline.plot({
+            "data":n[0],
+            "layout":layout
+        }, filename=n[1])
 
-
-# def gen_plotly_specs(data, name):
-#     trace = go.Scatter3d(
-#             x=data[:, 0],
-#             y=data[:, 1],
-#             z=data[:, 2],
-#             mode='markers',
-#             marker=dict(
-#                 size=12,
-#                 line=dict(
-#                     width=0.0
-#                 ),
-#                 opacity=0.8
-#             ),
-#             name=name
-#         )
-
-    return trace
-
-if __name__ == '__main__':
-    # need to go into the main py
-    # set_credentials_file(username='bakeralex664', api_key='hWwBstLnNCX5CsDZpOSU')
-    data = np.random.rand(50, 20)
-    cat = np.arange(1, 10)
-    print cat.shape
-    # lda_labels = ['poltics', 'sports']
-    # topics = np.arange(20)
-    # model = TSNE(n_components=3, random_state=0)
-    # tsne(data, ['one', 'two', 'three'], model)
-
-    # To make your color choice reproducible, uncomment the following line:
-    # random.seed(10)
-
-    # colors = []
-    #
-    # for i in range(0, 10):
-    #     colors.append(generate_new_color(colors, pastel_factor=0.9))
-    #
-    # print 'Your colors:',colors
